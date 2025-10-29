035329759c29846e8b633a92fdb4e72e446dd5e2
Rob Morgan <robleemorgan@gmail.com>
2025-10-29 15:26:15 +0800

    cleaned up some dependencies

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.scdn.co",
+        hostname: "*.scdn.co",
       },
     ],
+    formats: ["image/avif", "image/webp"],
   },
 };
 
